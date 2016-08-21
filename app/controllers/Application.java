eac2cc9801d5ce35386766a48c69cab3df9d9831
Live session user <sandeep.k@betsol.com>
2016-08-21 09:30:37 +0000

    First Lambda Done

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -3,6 +3,7 @@ package controllers;
 
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import forms.LoginForm;
 import forms.SignupForm;
 import models.Profile;
 import models.User;
@@ -16,6 +17,7 @@ import javax.inject.Inject;
 /**
  * Created by lubuntu on 8/21/16.
  */
+
 public class Application extends Controller {
     @Inject
     FormFactory formFactory;
@@ -23,18 +25,30 @@ public class Application extends Controller {
     ObjectMapper objectMapper;
 
     public Result signup(){
-        Form<SignupForm>form = formFactory.form(SignupForm.class).bindFromRequest();
-        if(form.hasErrors())
+        Form<SignupForm>sform = formFactory.form(SignupForm.class).bindFromRequest();
+        if(sform.hasErrors())
         {
 
-            return ok(form.errorsAsJson());
+            return ok(sform.errorsAsJson());
         }
-        Profile profile=new Profile(form.data().get("firstName"),form.data().get("lastName"));
+        Profile profile=new Profile(sform.data().get("firstName"),sform.data().get("lastName"));
         Profile.db().save(profile);
-        User user=new User(form.data().get("email"),form.data().get("password"));
+        User user=new User(sform.data().get("email"),sform.data().get("password"));
         user.profile=profile;
         User.db().save(user);
         return ok((JsonNode)objectMapper.valueToTree(user));
     }
+
+    public Result login()
+    {
+     Form<LoginForm>lform = formFactory.form(LoginForm.class).bindFromRequest();
+        if(lform.hasErrors())
+        {
+            return ok(lform.errorsAsJson());
+        }
+
+        User user=new User(lform.data().get("email"),lform.data().get("password"));
+        return ok((JsonNode)objectMapper.valueToTree(user));
+     }
 
 }
